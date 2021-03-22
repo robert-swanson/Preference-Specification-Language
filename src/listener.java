7859173e0ad8831a90d7796e82d63aa9c4064133
Liz Yeiter <elizabeth_yeiter@taylor.edu>
2021-03-22 10:50:34 -0400

    listener file created and working

---
+++ src/listener.java
@@ -0,0 +1,118 @@
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+
+public class listener extends PSLGrammarBaseListener {
+
+    @Override public void enterStart(PSLGrammarParser.StartContext ctx) { }
+
+    @Override public void exitStart(PSLGrammarParser.StartContext ctx) { }
+
+
+    @Override public void enterBlock(PSLGrammarParser.BlockContext ctx) { }
+
+    @Override public void exitBlock(PSLGrammarParser.BlockContext ctx) { }
+
+
+    @Override public void enterBody(PSLGrammarParser.BodyContext ctx) { }
+
+    @Override public void exitBody(PSLGrammarParser.BodyContext ctx) { }
+
+
+    @Override public void enterStatement(PSLGrammarParser.StatementContext ctx) { }
+
+    @Override public void exitStatement(PSLGrammarParser.StatementContext ctx) { }
+
+
+    @Override public void enterPriority(PSLGrammarParser.PriorityContext ctx) { }
+
+    @Override public void exitPriority(PSLGrammarParser.PriorityContext ctx) { }
+
+
+    @Override public void enterRequire(PSLGrammarParser.RequireContext ctx) { }
+
+    @Override public void exitRequire(PSLGrammarParser.RequireContext ctx) { }
+
+
+    @Override public void enterPrefer(PSLGrammarParser.PreferContext ctx) { }
+
+    @Override public void exitPrefer(PSLGrammarParser.PreferContext ctx) { }
+
+
+    @Override public void enterIf_(PSLGrammarParser.If_Context ctx) { }
+
+    @Override public void exitIf_(PSLGrammarParser.If_Context ctx) { }
+
+
+    @Override public void enterOtherwiseIf(PSLGrammarParser.OtherwiseIfContext ctx) { }
+
+    @Override public void exitOtherwiseIf(PSLGrammarParser.OtherwiseIfContext ctx) { }
+
+
+    @Override public void enterOtherwise(PSLGrammarParser.OtherwiseContext ctx) { }
+
+    @Override public void exitOtherwise(PSLGrammarParser.OtherwiseContext ctx) { }
+
+
+    @Override public void enterWhen(PSLGrammarParser.WhenContext ctx) { }
+
+    @Override public void exitWhen(PSLGrammarParser.WhenContext ctx) { }
+
+
+    @Override public void enterCondition(PSLGrammarParser.ConditionContext ctx) { }
+
+    @Override public void exitCondition(PSLGrammarParser.ConditionContext ctx) { }
+
+
+    @Override public void enterConstraint(PSLGrammarParser.ConstraintContext ctx) { }
+
+    @Override public void exitConstraint(PSLGrammarParser.ConstraintContext ctx) { }
+
+
+    @Override public void enterCourseNameList(PSLGrammarParser.CourseNameListContext ctx) { }
+
+    @Override public void exitCourseNameList(PSLGrammarParser.CourseNameListContext ctx) { }
+
+
+    @Override public void enterCourseName(PSLGrammarParser.CourseNameContext ctx) { }
+
+    @Override public void exitCourseName(PSLGrammarParser.CourseNameContext ctx) { }
+
+
+    @Override public void enterCredit_hours(PSLGrammarParser.Credit_hoursContext ctx) { }
+
+    @Override public void exitCredit_hours(PSLGrammarParser.Credit_hoursContext ctx) { }
+
+
+    @Override public void enterCourse_classes(PSLGrammarParser.Course_classesContext ctx) { }
+
+    @Override public void exitCourse_classes(PSLGrammarParser.Course_classesContext ctx) { }
+
+
+    @Override public void enterEveryRule(ParserRuleContext ctx) { }
+
+    @Override public void exitEveryRule(ParserRuleContext ctx) { }
+
+
+    @Override public void visitTerminal(TerminalNode node) { }
+
+    @Override public void visitErrorNode(ErrorNode node) { }
+
+
+    static public void main(String[] args) throws Exception {
+        CharStream input = CharStreams.fromFileName("inputs/bad/bad-and.psl");
+        PSLGrammarLexer lexer = new PSLGrammarLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        PSLGrammarParser parser = new PSLGrammarParser(tokens);
+
+        listener myListener = new listener();
+        parser.addParseListener(myListener);
+
+        parser.start();
+
+    }
+}
