1002cac3661442a4dfc87fead21f11f7578fce1c
rswanson <robert_swanson@taylor.edu>
2021-03-28 22:19:23 -0400

    objectified everything

---
+++ test-data/generator/output/Helpers/ContextStack.py
@@ -0,0 +1,32 @@
+import copy
+
+class ContextStack:
+    def __init__(self):
+        self.stack = []
+
+    def currentContext(self):
+        return self.stack[-1]
+    def pushContext(self, context):
+        self.stack.append(context)
+    def popContext(self):
+        return self.stack.pop()
+
+    def tryPushSubContextWithCondition(self, conditionFunction):
+        contextOut = copy.deepcopy(self.currentContext())
+        emptyContext = True
+        for term in self.currentContext()['terms']:
+            if not conditionFunction(self.contextFromTerm(term)):
+                contextOut['terms'].remove(term)
+            else:
+                emptyContext = False
+        if emptyContext:
+            return False
+        else:
+            self.pushContext(contextOut)
+            return True
+
+    def contextFromTerm(self, term):
+        return {
+            "terms": [term]
+        }
+
--- test-data/generator/output/evaluator.py
+++ test-data/generator/output/evaluator.py
@@ -1,24 +1,27 @@
-import helpers as hp
+from Helpers.PlanEvaluator import PlanEvaluator
+from Helpers.Validator import Validator
+import Helpers.Utilities as ut
 
-def validatePlan(plan):
-    hp.planIsValid = True
-    hp.assertTrue(hp.courseNameInContext(plan, "COS 120"),"COS 120 not scheduled")
-    hp.assertTrue(hp.courseNameInContext(plan, "COS 121"),"COS 121 not scheduled")
-    hp.assertTrue(hp.nCourseNamesInContext(plan, 2, ["COS 120", "COS 121", "COS 143"]),'Not enough courses taken from set: ["COS 120", "COS 121", "COS 143"]')
-    hp.assertTrue(hp.violatesLeftBeforeRight(plan, "COS 120", "COS 121"),"COS 120 must come before COS 121")
-    hp.assertFalse(hp.violatesLeftBeforeRight(plan, "COS 121", "COS 120"),"Assert failed: COS 120 must come before COS 121")
-    hp.assertEquals(hp.totalCredits(plan), 20, "Expected 20 credits")
-    hp.assertEquals(hp.averageStartTime(plan), 637.5, "Expected the minutes since midnight to be 637.5")
-    hp.assertEquals(hp.minsSinceMidnightToArmyTime(hp.averageStartTime(plan)), 1037, "expected army time to be 1037")
-    hp.assertEquals(hp.totalCreditsGreaterThanEqualToCourseNumber(plan, 200), 6, "expected 6 credits")
-    hp.assertEquals(hp.totalCoursesGreaterThanEqualToCourseNumber(plan, 200), 2, "expected 2 courses")
-    print(hp.contextFromSemester(plan, containsCOS120))
+def validatePlan():
+    validator = Validator()
+    validator.assertTrue(evaluator.courseName("COS 120"),"COS 120 not scheduled")
+    validator.assertTrue(evaluator.courseName("COS 121"),"COS 121 not scheduled")
+    validator.assertTrue(evaluator.nCourseNamesIn(2, ["COS 120", "COS 121", "COS 143"]),'Not enough courses taken from set: ["COS 120", "COS 121", "COS 143"]')
+    validator.assertTrue(evaluator.violatesLeftBeforeRight("COS 121", "COS 121"),"COS 120 must come before COS 121")
+    validator.assertFalse(evaluator.violatesLeftBeforeRight("COS 121", "COS 120"),"Assert failed: COS 120 must come before COS 121")
+    validator.assertEquals(evaluator.totalCredits(), 20, "Expected 20 credits")
+    validator.assertEquals(evaluator.averageStartTime(), 637.5, "Expected the minutes since midnight to be 637.5")
+    validator.assertEquals(ut.minsSinceMidnightToArmyTime(evaluator.averageStartTime()), 1037, "expected army time to be 1037")
+    validator.assertEquals(evaluator.totalCreditsGreaterThanEqualToCourseNumber(200), 6, "expected 6 credits")
+    validator.assertEquals(evaluator.totalCoursesGreaterThanEqualToCourseNumber(200), 2, "expected 2 courses")
 
-def containsCOS120(term):
-    return hp.courseNameInContext(plan, "COS 120")
+    if evaluator.contextStack.tryPushSubContextWithCondition(lambda contextIn: PlanEvaluator.courseNameInContext(contextIn, "COS 120")):
+        print(evaluator.contextStack.currentContext())
+        # Do context things
+        evaluator.contextStack.popContext()
 
-plan = hp.loadPlan()
-validatePlan(plan)
+    if validator.isValid:
+        print("Valid Plan")
 
-if hp.planIsValid:
-    print("Valid Plan")
+evaluator = PlanEvaluator()
+validatePlan()
